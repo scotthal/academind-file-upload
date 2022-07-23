ada05d323a591ef09339074d45ba435d8c7c43ee
Scott Hallock <scott@hallocks.net>
2022-07-23 22:48:53 +0000

    Fix Mongo connection settings

--- data/database.js
+++ data/database.js
@@ -1,17 +1,24 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('file-demo');
+  if (!process.env.MONGODB_USERNAME || !process.env.MONGODB_PASSWORD) {
+    console.error("Set MONGODB_USERNAME and MONGODB_PASSWORD!");
+    process.exit(-1);
+  }
+
+  const client = await MongoClient.connect(
+    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongo`
+  );
+  database = client.db("file-demo");
 }
 
 function getDb() {
   if (!database) {
-    throw { message: 'Database not connected!' };
+    throw { message: "Database not connected!" };
   }
   return database;
 }
