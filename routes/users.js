485de68b3a36d1c2d0a90145e4f314813ad9d2a0
Scott Hallock <scott@hallocks.net>
2022-07-24 13:50:21 +0000

    Initial file upload and storage

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
 
+const upload = multer({ dest: "user-images" });
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get("/", function (req, res) {
+  res.render("profiles");
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.post("/profiles", upload.single("image"), function (req, res) {
+  const uploadedImageFile = req.file;
+  const userName = req.body.username;
+
+  console.log(userName);
+  console.log(uploadedImageFile);
+
+  res.redirect("/");
+});
+
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
 });
 
 module.exports = router;
